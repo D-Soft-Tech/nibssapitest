313c544e6df813c81e21dbfb3d9c2ba8165ce1b3
dsofttech <dynamicsofttechnology@gmail.com>
2022-11-22 09:09:04 +0100

    increased the writeTimeout to accomodate delayed response from the backend

--- nibssclient/src/main/java/com/netpluspay/nibssclient/network/StormApiClient.kt
+++ nibssclient/src/main/java/com/netpluspay/nibssclient/network/StormApiClient.kt
@@ -6,6 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class StormApiClient {
 
@@ -17,6 +18,9 @@ class StormApiClient {
             val loggingInterceptor = HttpLoggingInterceptor()
             loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
             okHttpClientBuilder.addInterceptor(loggingInterceptor)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
 
             return okHttpClientBuilder
         }
