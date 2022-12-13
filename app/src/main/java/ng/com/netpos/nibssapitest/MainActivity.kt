c30cf984598045786612d7bab941b1ac73e48b27
dsofttech <dynamicsofttechnology@gmail.com>
2022-12-13 21:54:42 +0100

    testing the solution

--- app/src/main/java/ng/com/netpos/nibssapitest/MainActivity.kt
+++ app/src/main/java/ng/com/netpos/nibssapitest/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.danbamitale.epmslib.entities.*
+import com.danbamitale.epmslib.entities.* // ktlint-disable no-wildcard-imports
 import com.google.gson.Gson
 import com.netpluspay.netpossdk.NetPosSdk
 import com.netpluspay.netpossdk.emv.CardReadResult
